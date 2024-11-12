9fc2299b3dd3ee3817c1c3b5e5ad7e53cbbb8a50
Kyle Penfound <kyle@dagger.io>
2024-11-12 11:46:37 -0500

    test with hosted runners

--- .dagger/main.go
+++ .dagger/main.go
@@ -4,11 +4,18 @@ package main
 
 import (
 	"context"
+
+	"dagger/kubecon-na-2024/internal/dagger"
 )
 
 type KubeconNa2024 struct{}
 
 // Returns the goreleaser test pipeline
 func (m *KubeconNa2024) Test(ctx context.Context) (string, error) {
-	return dag.Goreleaser().Test().Output(ctx)
+	source := dag.Git("github.com/goreleaser/goreleaser").
+		Commit("12155a336baaa705e9363e3001c048238f15ff34").
+		Tree()
+	return dag.Goreleaser(dagger.GoreleaserOpts{Source: source}).
+		Test().
+		Output(ctx)
 }
